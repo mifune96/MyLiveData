395a9c00068361a6477eb6805d145737b34b275d
mifune96 <imrank.sensea@gmail.com>
2021-12-30 08:18:55 +0700

    live data

---
+++ app/src/main/java/com/tomuchcoffee/mylivedata/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.tomuchcoffee.mylivedata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.tomuchcoffee.mylivedata.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mLiveDataTimerViewModel: MainViewModel
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mLiveDataTimerViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        subscribe()
+    }
+
+    private fun subscribe() {
+        val elapsedTimeObserver = Observer<Long?>{ aLong ->
+            val newText = this@MainActivity.resources.getString(R.string.seconds, aLong)
+            binding.timerTextview.text = newText
+        }
+        mLiveDataTimerViewModel.getElapsedTime().observe(this, elapsedTimeObserver)
+    }
+}
